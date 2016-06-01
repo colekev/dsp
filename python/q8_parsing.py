0bb158f358c21675083302fffe007752708f0443
colekev <colekev@Kevins-MacBook-Pro.local>
2016-06-01 14:04:54 -0400

    answered Q8

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,7 +1,7 @@
-#The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
+#The football.csv file contains the results from the English Premier League.
+# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of
+# goals scored for and against each team in that season (so Arsenal scored 79 goals
+# against opponents, and had 36 goals scored against them). Write a program to read the file,
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
 # The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
@@ -9,11 +9,27 @@
 
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+with open('football.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    goals = []
+    goalsAll = []
+    teams = []
+    for row in readCSV:
+        team = row[0]
+        goal = row[5]
+        goalAll = row[6]
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+        teams.append(team)
+        goals.append(goal)
+        goalsAll.append(goalAll)
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+    n = len(goals) - 1
+    goalDiff = []
+    for i in range(1,n):
+        diff = abs(int(goals[i]) - int(goalsAll[i]))
+        goalDiff.append(diff)
+
+    teamIndex = (goalDiff.index(min(goalDiff))) + 1
+    teamMinDiff = teams[teamIndex]
+
+    print(teamMinDiff)
