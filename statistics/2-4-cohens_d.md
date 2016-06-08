e83907954c8245491c118afdbf8e3ee15bb864ff
colekev <colekev@Kevins-MacBook-Pro.local>
2016-06-08 13:50:34 -0400

    adding Q1 files

--- statistics/2-4-cohens_d.md
+++ statistics/2-4-cohens_d.md
@@ -1,3 +1,7 @@
 [Think Stats Chapter 2 Exercise 4](http://greenteapress.com/thinkstats2/html/thinkstats2003.html#toc24) (Cohen's d)
 
->> REPLACE THIS TEXT WITH YOUR RESPONSE
+>> First babies are lighter (mean 7.20 lbs) than others (mean 7.32 lbs).
+
+>> The Cohen's d between the two groups is -0.089 (pos 0.089 if you make the heavier "others" group1).
+
+>> The effect size, as measured by Cohen's d, is larger for birth weight than for pregnancy length (0.29), though not particularly significant.
---
+++ statistics/2-4-cohens_d.py
@@ -0,0 +1,40 @@
+"""This file contains code for use with "Think Stats",
+by Allen B. Downey, available from greenteapress.com
+
+Copyright 2014 Allen B. Downey
+License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+"""
+
+from __future__ import print_function
+
+import numpy as np
+import sys
+
+import nsfg
+import thinkstats2
+import math
+
+preg = nsfg.ReadFemPreg()
+live = preg[preg.outcome == 1]
+
+firsts = live[live.birthord == 1]
+others = live[live.birthord != 1]
+
+meanFirsts = firsts.totalwgt_lb.mean()
+meanOthers = others.totalwgt_lb.mean()
+
+print(meanFirsts)
+print(meanOthers)
+
+def CohenEffectSize(group1, group2):
+    diff = group1.mean() - group2.mean()
+
+    var1 = group1.var()
+    var2 = group2.var()
+    n1, n2 = len(group1), len(group2)
+
+    pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+    d = diff / math.sqrt(pooled_var)
+    print(d)
+    
+CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb)
